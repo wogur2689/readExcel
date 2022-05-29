388e8949c6d3c4e52602a01c32a49bce328adb7c
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-05-30 01:18:55 +0900

    컨트롤, 리포지토리, 서비스 추가

---
+++ src/main/java/com/example/readexcel/entity/Common.java
@@ -0,0 +1,24 @@
+package com.example.readexcel.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Common {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt; //생성일자
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt; //수정일자
+}
